cfcea750785947ac916f13ee95fe6106a85a9ae5
Fredi Kats <fredikats@outlook.com>
2022-05-21 10:05:25 +0300

    Add benchmark

---
+++ GeneticAlgo.Benchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using GeneticAlgo.Shared;
+using GeneticAlgo.Shared.Entities;
+
+Summary? summary = BenchmarkRunner.Run<FileSystemBenchmark>();
+Console.Read();
+
+[SimpleJob]
+[MemoryDiagnoser]
+public class FileSystemBenchmark
+{
+    [Params(true, false)]
+    public bool UseShared { get; set; }
+
+    [Benchmark]
+    public void Read()
+    {
+        AllocationMode.UseShared = UseShared;
+
+        const int iterationCount = 10;
+        var execution = new Execution(4, 4, 1000, 500);
+
+        int restartCount = iterationCount;
+        while (restartCount > 0)
+        {
+            execution.ExecuteIteration();
+            if (execution.Population.AllDead())
+            {
+                execution.ExecuteIfDead();
+                restartCount--;
+            }
+        }
+    }
+}
